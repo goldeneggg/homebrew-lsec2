179ff1081b7edd4756bfb6dd37c3cb29fa35289c
goldeneggg <jpshadowapps@gmail.com>
2016-09-24 19:29:03 +0900

    bump up to 0.0.2

--- lsec2.rb
+++ lsec2.rb
@@ -3,14 +3,14 @@ require "formula"
 class Lsec2 < Formula
   if Hardware::CPU.is_32_bit?
     url 'https://drone.io/github.com/goldeneggg/lsec2/files/artifacts/bin/darwin_386/lsec2'
-    sha256 '933ee34a1e1f85b008a577916453ee397e34fc0786c85628d987b3ca0f9b0744'
+    sha256 '21554888234b2582cf37537193c2c971ca5a26612e7ada21a4d2ee6a7eb7f785'
   else
     url 'https://drone.io/github.com/goldeneggg/lsec2/files/artifacts/bin/darwin_amd64/lsec2'
-    sha256 'ca7b6a91bda4a81839f307b92a7c74f2550cf6ef946fb14e2582314b100a0556'
+    sha256 '59ff6920e3d2bcfd2886db484586933275f49a14fc4b2ce407b0d4e8acf91768'
   end
 
   homepage 'https://github.com/goldeneggg/lsec2'
-  version '0.0.1'
+  version '0.0.2'
 
   def install
     bin.install 'lsec2'
